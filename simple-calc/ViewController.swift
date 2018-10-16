c0f4cabfa4bbb8d585b23087fdff2150a311b75b
Nattanon Bunyatipanon <nattanon33@yahoo.com>
2018-10-16 14:35:46 -0700

    Initial Commit

---
+++ simple-calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simple-calc
+//
+//  Created by Nattanon Bunyatipanon on 10/16/18.
+//  Copyright © 2018 Nattanon Bunyatipanon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
